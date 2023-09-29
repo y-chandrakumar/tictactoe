5f67a51932774fbabf48f5c5a8261d072788b62d
y-chandrakumar <142469091+y-chandrakumar@users.noreply.github.com>
2023-09-29 22:20:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const single = document.getElementById("btn-one");
 const multi = document.getElementById("btn-two");
 const easyBtn = document.getElementById("easy-btn");
+const hardBtn = document.getElementById("hard-btn");
 let matter=document.getElementById("text");
-
 const gameDiv = document.querySelector(".game-div");
 const submitBtn = document.getElementById("submit-btn");
 const grid = document.querySelector(".grid");
 const cells = document.querySelectorAll(".cell");
 const btn = document.getElementById("albtn");
+let hard=true;
 let singleplaying=true;
-let currentPlayer = "X"; // User starts
+let currentPlayer = "X"; 
 let isGameOver = false;
-function showcel(cels){
-    console.log("  {  ");
-    for(let i=0;i<9;i++){
-        console.log(cels[i].textContent);
-    }
-    console.log("  }  ");
-}
  
+
+const board = ['', '', '', '', '', '', '', '', ''];
+
+// Define the AI player and opponent
+const aiPlayer = 'O';
+const opponent = 'X';
 // Function to create the Tic-Tac-Toe grid
 function createTicTacToeGrid() {
     btn.style.display="none";
@@ -50,7 +50,7 @@ function checkGameOver(won) {
         if ((cels[a].textContent==="X" || cels[a].textContent==="O") &&
 
         cels[a].textContent === cels[b].textContent && cels[a].textContent === cels[c].textContent) {
-            console.log(a,b,c);
+          
          isGameOver = true;
          if(won){
             
@@ -160,38 +160,131 @@ function blockmove(){
 }
 
 function computerMove() {
-    let num=easymove();
     let randomIndex=0;
+    const cels = document.querySelectorAll(".cell");   
+ 
+    for(let i=0;i<9;i++){
+        board[i]=cels[i].textContent;
+        
+    }
+    if(hard ){
+        randomIndex = findBestMove(board);
+       
+    }
+    else{
+    let num=easymove();
     if(num>=0){
         randomIndex=num; 
+       
     }
     else if(num<0){
-    let bloc=blockmove();
-    if(bloc>=0){
-        randomIndex=bloc;
+        let bloc=blockmove();
+        if(bloc>=0){
+            randomIndex=bloc;
+            
     }}
     else{
      randomIndex = Math.floor(Math.random() * 8);
+     
     }
-    const cels = document.querySelectorAll(".cell");   
   
     while(cels[randomIndex].textContent){
         randomIndex = Math.floor(Math.random() * 9);   
     }
      
-    cels[randomIndex].textContent = "O";
-   
     
+}
+cels[randomIndex].textContent = "O";
             currentPlayer = "X";
             matter.innerHTML=currentPlayer+"'s  Turn";
             checkGameOver(true,-1);
             
             
         }
-        
-        
-        
-        
+ 
+function checkWinner(board, player) {
+    const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (const pattern of winPatterns) {
+        if (pattern.every(i => board[i] === player)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Minimax function
+function minimax(board, depth, isMaximizing) {
+    
+    const scores = {
+        X: -1,
+        O: 1,
+        tie: 0
+    };
+
+    if (checkWinner(board, aiPlayer)) {
+        return scores.O;
+    } else if (checkWinner(board, opponent)) {
+        return scores.X;
+    } else if (board.every(cell => cell !== '')) {
+        return scores.tie;
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === '') {
+                board[i] = aiPlayer;
+                const score = minimax(board, depth + 1, false);
+                board[i] = '';
+                bestScore = Math.max(score, bestScore);
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === '') {
+                board[i] = opponent;
+                const score = minimax(board, depth + 1, true);
+                board[i] = '';
+                bestScore = Math.min(score, bestScore);
+            }
+        }
+        return bestScore;
+    }
+}
+
+// Find the optimal move for the AI player
+function findBestMove(board) {
+  
+    let bestMove = -1;
+    let bestScore = -Infinity;
+
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === '') {
+            board[i] = aiPlayer;
+            const score = minimax(board, 0, false);
+            board[i] = '';
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = i;
+            }
+        }
+    }
+
+    return bestMove;
+}
+
         // Event listeners
         grid.addEventListener("click", (event) => {
             if (!isGameOver && event.target.classList.contains("cell") && !event.target.textContent) {
@@ -216,11 +309,11 @@ function computerMove() {
             }
         });
         
-       
         single.addEventListener("click",()=>{
             single.style.display="none";
             multi.style.display="none";
             easyBtn.style.display="block";
+            hardBtn.style.display="block";
           
         })
         multi.addEventListener("click",()=>{
@@ -230,5 +323,12 @@ function computerMove() {
         })
         easyBtn.addEventListener("click", () => {
             createTicTacToeGrid();
+            hard=false;
+        });
+
+        hardBtn.addEventListener("click", () => {
+            createTicTacToeGrid();
+            hard=true;
+            
         });
         
